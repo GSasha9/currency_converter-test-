57f9fac8fea3c7ee479e8a829efea4d103840fbb
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2025-03-03 10:43:38 +0300

    refactor: updated the exchange rates

--- src/components/constants/currency-details.ts
+++ src/components/constants/currency-details.ts
@@ -4,26 +4,26 @@ export const CURRENCY_DETAILS: Record<CurrencyCode, CurrencyData> = {
     BYN: {
         Abbreviation: 'BYN',
         Name: 'Belarusian ruble',
-        Rates: { USD: 0.39, EUR: 0.36, CNY: 2.57, RUB: 29.45, BYN: 1 }
+        Rates: { USD: 0.31, EUR: 0.30, CNY: 2.3, RUB: 27.70, BYN: 1 }
     },
     USD: {
         Abbreviation: 'USD',
         Name: 'Dollar',
-        Rates: { BYN: 2.56, EUR: 0.92, CNY: 6.68, RUB: 75.34, USD: 1 }
+        Rates: { BYN: 3.2, EUR: 0.96, CNY: 7.38, RUB: 88.34, USD: 1 }
     },
     EUR: {
         Abbreviation: 'EUR',
         Name: 'Euro',
-        Rates: { USD: 1.09, BYN: 3.78, CNY: 7.72, RUB: 82.25, EUR: 1 }
+        Rates: { USD: 1.03, BYN: 3.33, CNY: 7.67, RUB: 92.25, EUR: 1 }
     },
     CNY: {
         Abbreviation: 'CNY',
         Name: 'Yuan',
-        Rates: { USD: 0.15, EUR: 0.14, BYN: 0.39, RUB: 11.5, CNY: 1 }
+        Rates: { USD: 0.13, EUR: 0.13, BYN: 0.43, RUB: 12.01, CNY: 1 }
     },
     RUB: {
         Abbreviation: 'RUB',
         Name: 'Russian ruble',
-        Rates: { USD: 0.013, EUR: 0.012, CNY: 0.087, BYN: 0.034, RUB: 1 }
+        Rates: { USD: 0.013, EUR: 0.010, CNY: 0.083, BYN: 0.036, RUB: 1 }
     }
 };
